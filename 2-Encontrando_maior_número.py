74378558f41fbe6520ef7a30c3892bab718bc6a2
ycarofelipe <ycarofelipe551@gmail.com>
2025-08-06 10:05:38 -0300

    Solução concluída do "Encontrando o maior numero"

---
+++ 2-Encontrando_maior_número.py
@@ -0,0 +1,4 @@
+#Encontrando o Maior Número
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+maior_numero = max(numeros)
+print(maior_numero)
