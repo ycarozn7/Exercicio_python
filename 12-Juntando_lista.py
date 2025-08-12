0b5548645075c85fcabf2dd97d39378c466fdca0
ycarofelipe <ycarofelipe551@gmail.com>
2025-08-12 20:02:08 -0300

    Solução concluída do "Juntando Duas Listas"

---
+++ 12-Juntando_lista.py
@@ -0,0 +1,7 @@
+natureza = ["rio", "floresta", "leão", "montanha", "oceano", "estrela", "vento", "cachoeira", "lua", "girassol"]
+tecnologia = ["algoritmo", "API", "blockchain", "código", "software", "nuvem", "hardware", "interface", "pixel", "servidor"]
+
+def juntar(lista1, lista2):
+    return lista1 + lista2
+soma_lista = juntar(natureza, tecnologia)
+print(soma_lista)
