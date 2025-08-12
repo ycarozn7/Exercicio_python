b9fd1d16238260936c25b01f111bed51bd5e0d54
ycarofelipe <ycarofelipe551@gmail.com>
2025-08-12 20:07:03 -0300

    Solução concluída do "Retornando o menor número"

---
+++ 13-Retornando_menor_numero.py
@@ -0,0 +1,6 @@
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+def menor_numero(lista):
+    return min(lista)
+
+menor_numerop = menor_numero(numeros)
+print(menor_numerop)
