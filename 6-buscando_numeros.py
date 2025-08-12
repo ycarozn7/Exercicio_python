d132ca9a8aa9fe9521fa5f2cebcb1d5a6ab59ca9
ycarofelipe <ycarofelipe551@gmail.com>
2025-08-12 19:00:31 -0300

    Solução concluída do "Buscando um número"

---
+++ 6-buscando_numeros.py
@@ -0,0 +1,9 @@
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+
+def encontrar(numero, lista):
+  if numero in lista:
+        return True
+  else:
+        return False
+procurar_numero = encontrar(88888, numeros)
+print(procurar_numero)
