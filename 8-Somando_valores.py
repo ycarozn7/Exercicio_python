b80139cf5185206a0a482cb3617f70486d926295
ycarofelipe <ycarofelipe551@gmail.com>
2025-08-12 19:24:26 -0300

    Solução concluída do "Somando os valores da lista"

---
+++ 8-Somando_valores.py
@@ -0,0 +1,5 @@
+def soma_lista(lista):
+    return sum(lista)
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+soma = soma_lista(numeros)
+print(soma)
