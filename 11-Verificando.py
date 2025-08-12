9754c580dbb723ce3cfb14ea0d4e3c5aeab05326
ycarofelipe <ycarofelipe551@gmail.com>
2025-08-12 19:58:25 -0300

    Solução concluída do "Verificando se a Lista está Vazia"

---
+++ 11-Verificando.py
@@ -0,0 +1,9 @@
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+def verificação(lista):
+    if lista == []:
+       return True
+    else:
+       return False
+lista_vazia = verificação(numeros)
+print(lista_vazia)
